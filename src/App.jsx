5ed69665ce094dc21b35c44520609a434dddd92d
joel aviles <joelaviles413@gmail.com>
2024-03-12 23:02:21 -0600

    start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// Supports weights 200-800
+import '@fontsource-variable/manrope';
+
+function App() {
+
+  return (
+    <main className="h-screen bg-darkGrayishBlue flex justify-center items-center">
+      <article className='w-[343px] h-[315px] bg-grayishBlue flex flex-col justify-center'>
+      <img src="./assets/" alt="" sizes="" srcset="" />
+        <button>
+
+        </button>
+      </article>
+    </main>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'lightCyan': '#CEE3E9',
+      'neonGreen': '#52FFA8',
+      'grayishBlue': '#4E5D73',
+      'darkGrayishBlue': '#323A49',
+      'darkBlue': '#1F2632'
+    },
+    extend: {
+      fontFamily: {
+        manrope: 'manrope'
+      }
+    }
+  },
+  plugins: [],
+}
