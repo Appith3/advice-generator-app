503d0221be9b41e4271b1a06736d106175acd1ad
joel aviles <joelaviles413@gmail.com>
2024-06-12 21:51:24 -0600

    move svg's path

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,9 @@ function App() {
       <article className='w-[343px] desktop:w-[540px] h-[315px] desktop:h-[332px] bg-darkGrayishBlue flex flex-col justify-center items-center rounded-lg p-6 desktop:p-12 relative gap-6'>
         <h1 className='uppercase text-neonGreen font-extrabold tracking-[.24em] desktop:tracking-[.32em] text-xs desktop:text-[13px]'>advice #{advice.adviceID}</h1>
         <span className='text-lightCyan font-extrabold leading-8 desktop:leading-10 text-[24px] desktop:text-[27px] text-center'>“{advice.advice}”</span>
-        <img className='w-full' src="src/assets/pattern-divider-mobile.svg" srcSet="src/assets/pattern-divider-desktop.svg" />
+        <img className='w-full' src="/pattern-divider-mobile.svg" srcSet="/pattern-divider-desktop.svg" />
         <button className='bg-neonGreen w-16 h-16 flex justify-center items-center rounded-full absolute -bottom-8 hover:drop-shadow-[0_0_32px_#52FFA8]' onClick={getAdvice}>
-          <img src="src/assets/icon-dice.svg" alt="dice icon" />
+          <img src="/icon-dice.svg" alt="dice icon" />
         </button>
       </article>
     </main>
